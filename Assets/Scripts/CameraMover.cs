bf11bac1c662cb5f2b1659d3ab0cda682c41266e
Takeshi Kaku <t.kaku710@gmail.com>
2020-12-04 19:22:10 +0900

    Initial Commit

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+   public Transform playerTransform;
+    void Update()
+    {
+        transform.position = playerTransform.position + new Vector3(0, 5.5f, -4.5f);
+    }
+}
---
+++ Assets/Scripts/SantaRotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class SantaRotator : MonoBehaviour
+{
+    Vector3 latestPos;
+
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                Vector3 diff = transform.position - latestPos;
+
+                if (diff.magnitude > 0.01f)
+                {
+                    transform.rotation = Quaternion.LookRotation(diff);
+                }
+
+                latestPos = transform.position;
+            }).AddTo(gameObject);
+    }
+}
